5f1c20218b48bc43ae272f4ab7d1daa5d4dc27df
Donny Roufs <iamchets@yahoo.com>
2020-04-02 21:55:45 +0200

    moved the guilds dispatch to the home page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import Layout from "./components/Layout";
 import * as Page from "./pages";
-import { getGuilds } from "./redux/actions/guild.actions";
 
 /* eslint-disable */
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getGuilds());
-  }, []);
-
   return (
     <div className="App">
       <Router>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getGuilds } from "../redux/actions/guild.actions";
+
 import { Card } from "../components/index";
 import { Flex } from "@chakra-ui/core";
 import { Loader } from "../components";
 
-const Home = () => {
+const Home = ({ match }) => {
+  const dispatch = useDispatch();
+
   const guilds = useSelector(state => state.guild.guilds);
   const loading = useSelector(state => state.guild.loading);
+
+  useEffect(() => {
+    dispatch(getGuilds());
+  }, [dispatch, match.url]);
 
   if (loading) {
     return <Loader />;
