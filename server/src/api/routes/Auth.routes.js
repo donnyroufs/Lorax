ff1bacc813341e015dad8aec1f6c5744be587980
iamchets <iamchets@yahoo.com>
2020-04-15 12:43:06 +0200

    added routing and packages for oauth2

---
+++ server/src/api/routes/Auth.routes.js
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import passport from "passport";
+import { Strategy as DiscordStrategy } from "passport-discord";
+
+const router = Router();
+
+const scopes = ["identify"];
+
+router.get(
+  "/",
+  passport.authenticate("discord", { scope: scopes }),
+  (req, res) => {
+    res.send("hello world");
+  }
+);
+
+router.get(
+  "/callback",
+  passport.authenticate("discord", { failureRedirect: "/" }),
+  (req, res) => {
+    res.send("This is the callback endpoint");
+  }
+);
+
+passport.use(
+  new DiscordStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+      scope: scopes,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      process.nextTick(function () {
+        return done(null, profile);
+      });
+    }
+  )
+);
+
+export default router;
