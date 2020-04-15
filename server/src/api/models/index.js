f54a7b737c042b4d9472acd136a15705b676b7ac
iamchets <iamchets@yahoo.com>
2020-04-15 12:17:40 +0200

    Changed env variables, so that I dont have to keep track of 2 seperate variables in dev/prod envs.

--- server/src/api/models/index.js
+++ server/src/api/models/index.js
@@ -1,9 +1,9 @@
 import Sequelize from "sequelize";
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
@@ -11,8 +11,8 @@ const sequelize = new Sequelize(
     pool: {
       max: 5,
       min: 0,
-      idle: 10000
-    }
+      idle: 10000,
+    },
   }
 );
 
@@ -20,10 +20,10 @@ const models = {
   Guild: sequelize.import("./Guild.model"),
   User: sequelize.import("./User.model"),
   Question: sequelize.import("./Question.model"),
-  Answer: sequelize.import("./Answer.model")
+  Answer: sequelize.import("./Answer.model"),
 };
 
-Object.keys(models).forEach(modelName => {
+Object.keys(models).forEach((modelName) => {
   if ("associate" in models[modelName]) {
     models[modelName].associate(models);
   }
